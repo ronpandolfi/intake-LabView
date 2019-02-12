65b4e6c16ffc3374d92d2f2108d237c315bbb581
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-02-11 16:38:44 -0800

    Add dask composition

--- labviewdatasource.py
+++ labviewdatasource.py
@@ -1,6 +1,7 @@
 import numpy as np
 import intake
 from collections import defaultdict
+from dask import array as da
 
 
 def convertDate(s):
@@ -38,17 +39,17 @@ class LabViewLogSource(intake.source.base.DataSource):
     def _get_partition(self, i):
         # Return the appropriate container of data here
         # TODO: convert to dask bag from_array
-        return np.genfromtxt(self.partitions[i],
+        return da.from_array(np.genfromtxt(self.partitions[i],
                        delimiter = '\t',
                        names=True,
                        invalid_raise=False,
                        # converters = {0: lambda s:  convertDate(s)},
-                       dtype=tuple([np.dtype('U32')]+[float]*50))
+                       dtype=tuple([np.dtype('U32')]+[float]*50)),1)
 
     def read(self):
         # self._load_metadata()
         # TODO: change to dask.concatenate
-        return np.concatenate([self.read_partition(i) for i in range(self.npartitions)])
+        return da.concatenate([self.read_partition(i) for i in range(self.npartitions)])
 
     def _close(self):
         # close any files, sockets, etc
@@ -58,5 +59,5 @@ class LabViewLogSource(intake.source.base.DataSource):
 
 if __name__ == '__main__':
     catalog = LabViewLogSource(['/home/rp/Downloads/04-01-2018 Data.txt'])
-    print(catalog.read())
+    print(catalog.read()[0].compute())
 
